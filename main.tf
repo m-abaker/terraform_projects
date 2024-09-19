071f0026317844c27bcbd6c0f857530e83544eee
m-abaker <117376300+m-abaker@users.noreply.github.com>
2024-09-19 19:06:48 +0300

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-# Create IAM users
-resource "aws_iam_user" "lb" {
-  name  = "loadbalancer${count.index + 1}"
-  path  = "/system/"
-  count = 3
-  tags = {
-    tag-key = "tag-value${count.index + 1}"
-  }
-}
