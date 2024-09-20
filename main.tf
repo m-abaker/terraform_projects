0545ce98b091bd640dfbfa562fa86beb79a74a52
m-abaker <m.abaker124@gmail.com>
2024-09-20 17:30:14 +0300

    create S3 & save there state file

--- main.tf
+++ main.tf
@@ -1,9 +1,29 @@
-# Create IAM users
-resource "aws_iam_user" "lb" {
-  name  = "loadbalancer${count.index + 1}"
-  path  = "/system/"
-  count = 3
+# # Create IAM users
+# resource "aws_iam_user" "lb" {
+#   name  = "loadbalancer${count.index + 1}"
+#   path  = "/system/"
+#   count = 3
+#   tags = {
+#     tag-key = "tag-value${count.index + 1}"
+#   }
+# }
+
+# Create EC2 
+resource "aws_instance" "web" {
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+
   tags = {
-    tag-key = "tag-value${count.index + 1}"
+    Name = "HelloWorld"
+  }
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket124"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,18 +1,23 @@
 terraform {
-  required_providers {
+    required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
+    }
+    backend "s3" {
+    bucket = "my-tf-test-bucket124"
+    key    = "network/terraform.tfstate"
+    region = "us-east-1"
+    }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["/Users/mabak/.aws/config"]
-  shared_credentials_files = ["/Users/mabak/.aws/credentials"]
-  profile                  = "ass5"
+  region = "us-east-1"
+  #shared_config_files      = ["~/.aws/config"]
+  # shared_credentials_files = ["~/.aws/credentials"]
+  profile = "ass5"
 }
 
 #############################################################################
