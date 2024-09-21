6694c1d9ee609839f2ad3a828e5107ee4701e1c1
m-abaker <117376300+m-abaker@users.noreply.github.com>
2024-09-21 14:53:40 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,21 @@
-terraform {
+/*terraform {
     required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
     }
-    /*backend "s3" {
+    backend "s3" {
     bucket = "my-tf-test-bucket124"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
-    }*/
-}
+    }
+}*/
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  #profile = "ass5"
-}
+ }
 
 #############################################################################
 
