cbe54f226befeadad576df30d79428f9037010f7
m-abaker <117376300+m-abaker@users.noreply.github.com>
2024-09-21 18:54:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
- # Create IAM users
+/* # Create IAM users
  resource "aws_iam_user" "lb" {
    name  = "loadbalancer${count.index + 1}"
    path  = "/system/"
@@ -6,16 +6,16 @@
    tags = {
      tag-key = "tag-value${count.index + 1}"
    }
- }
+ } */
 
 # Create EC2 
 resource "aws_instance" "web" {
   ami           = "ami-0e86e20dae9224db8"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+#  tags = {
+ #   Name = "HelloWorld"
+ # }
 }
 
 /*# Create S3 bucket
