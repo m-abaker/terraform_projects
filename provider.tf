3c69120ad78baca69cf93b392e7f08306f81eef4
m-abaker <117376300+m-abaker@users.noreply.github.com>
2024-09-21 14:35:48 +0300

    Update provider.tf
    commit S3 backend

--- provider.tf
+++ provider.tf
@@ -5,18 +5,16 @@ terraform {
       version = "~> 5.0"
     }
     }
-    backend "s3" {
+    /*backend "s3" {
     bucket = "my-tf-test-bucket124"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
-    }
+    }*/
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  #shared_config_files      = ["~/.aws/config"]
-  # shared_credentials_files = ["~/.aws/credentials"]
   profile = "ass5"
 }
 
